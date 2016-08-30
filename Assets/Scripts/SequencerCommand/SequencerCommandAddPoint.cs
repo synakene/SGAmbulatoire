e14ea38b2aa2ba721ce647a170dc9a5a414aaf11
Raphael Lehiany <r.lehiany@synakene.fr>
2016-08-30 15:06:57 +0200

    popup score

--- Assets/Scripts/SequencerCommand/SequencerCommandAddPoint.cs
+++ Assets/Scripts/SequencerCommand/SequencerCommandAddPoint.cs
@@ -17,20 +17,8 @@ namespace PixelCrushers.DialogueSystem.SequencerCommands {
                 int score2 = int.Parse(GetParameter(1));
                 int score3 = int.Parse(GetParameter(2));
 
-                Data.curScoreObj1 += score1;
-                if (Data.curScoreObj1 < 0) { Data.curScoreObj1 = 0; }
-                GameObject.Find("count1").GetComponent<Text>().text = (Data.curScoreObj1).ToString() + "/" + (Data.MaxScoreObj1).ToString();
-
-                Data.curScoreObj2 += score2;
-                if (Data.curScoreObj2 < 0) { Data.curScoreObj2 = 0; }
-                GameObject.Find("count2").GetComponent<Text>().text = (Data.curScoreObj2).ToString() + "/" + (Data.MaxScoreObj2).ToString();
-
-                Data.curScoreObj3 += score3;
-                if (Data.curScoreObj3 < 0) { Data.curScoreObj3 = 0; }
-                GameObject.Find("count3").GetComponent<Text>().text = (Data.curScoreObj3).ToString() + "/" + (Data.MaxScoreObj3).ToString();
-
-                GameObject ProfileButton = GameObject.Find("ProfileButton");
-                iTween.PunchScale(ProfileButton.gameObject, new Vector3(0.3f, 0.3f), 1.2f);
+                OverMind overmind = GameObject.Find("GameManager").GetComponent<OverMind>();
+                overmind.AddPoint(score1, score2, score3);
             }
 
             Stop();
---
+++ Assets/Scripts/UI/ScoreAnim.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreAnim : MonoBehaviour {
+
+    RectTransform rt;
+    Vector2 from;
+    Vector2 to;
+
+    void Start()
+    {
+        rt = gameObject.GetComponent<RectTransform>();
+        from = new Vector2(rt.anchoredPosition.x, 130f);
+        to = new Vector2(rt.anchoredPosition.x, 10f);
+    }
+
+    public void Move()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", from,
+            "to", to,
+            "time", 1f,
+            "easetype", "easeOutElastic",
+            "oncomplete", "Finish",
+            "onupdate", "Up"
+            ));
+    }
+
+    private void Up(Vector2 pos)
+    {
+        rt.anchoredPosition = pos;
+    }
+
+    public IEnumerator Finish()
+    {
+        yield return Wait(4);
+        iTween.MoveTo(gameObject, iTween.Hash(
+            "y", 80f,
+            "time", 1f,
+            "islocal", true
+        ));
+    }
+
+
+    public IEnumerator Wait(float t)
+    {
+        float curT = 0f;
+        while (curT < t)
+        {
+            curT += Time.deltaTime;
+            yield return 0;
+        }
+    }
+}
